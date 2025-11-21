180e2461170151dfae8ba9dce7efe492528c2c2b
Stas <uystas753@gmail.com>
2025-11-21 17:55:11 +0300

    test auth build
    для сборки сервиса auth вводим команды
    docker-compose build
    docker-compose run --rm auth-service
    
    если все збс, то остальные сервисы подобным образом бахну пока что

--- services/auth/db.py
+++ services/auth/db.py
@@ -1,8 +1,32 @@
 from pydantic_settings import BaseSettings
+from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.pool import NullPool
+import os
 
 class DatabaseSettings(BaseSettings):
-    database_url: str = "postgresql+asyncpg://auth_user:auth_pass@localhost:5432/chupapupa_pj"
-    schema_name: str = "auth_service"
+    database_url: str = os.getenv( "AUTH_DATABASE_URL",
+                "postgresql+asyncpg://auth_user:auth_pass@postgres:5432/chupapupa_pj")
+    sync_database_url: str = os.getenv( "AUTH_SYNC_DATABASE_URL",
+                "postgresql://auth_user:auth_pass@postgres:5432/chupapupa_pj")  # Для Alembic
+    schema_name: str = os.getenv("AUTH_SCHEMA_NAME", "auth_service")
 
 settings = DatabaseSettings()
 
+
+engine = create_async_engine(settings.database_url, future=True, poolclass=NullPool,
+                             connect_args={"server_settings": {"search_path": settings.schema_name}} )
+
+AsyncSessionLocal = sessionmaker(engine, class_=AsyncSession, expire_on_commit=False)
+
+
+async def get_db():
+    async with AsyncSessionLocal() as session:
+        try:
+            yield session
+            await session.commit()
+        except Exception:
+            await session.rollback()
+            raise
+        finally:
+            await session.close()
