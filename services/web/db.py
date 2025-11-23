7c7a00120ecac5e7a828a4f4fb7deb8688793e1e
Stas <uystas753@gmail.com>
2025-11-23 14:33:44 +0300

    вернул strict, удалил лишнее
    перед этим был pre-commit run
    --all-files, так что все четко, хуйни  0

--- services/web/db.py
+++
@@ -1,9 +0,0 @@
-from pydantic_settings import BaseSettings
-
-
-class DatabaseSettings(BaseSettings):
-    database_url: str = "postgresql+asyncpg://web_user:web_pass@localhost:5432/chupapupa_pj"
-    schema_name: str = "web_service"
-
-
-settings = DatabaseSettings()
