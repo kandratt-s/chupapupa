8b4c07ccf40c0eede9a8f64e9ad1a126715e45d6
Vllad3 <vlad.kiselv.2006@mail.ru>
2025-12-12 14:37:23 +0300

    id, role, hashed_password

--- services/auth/models.py
+++ services/auth/models.py
@@ -7,13 +7,5 @@ class User(Base):
     __tablename__ = "users"
 
     id = Column(Integer, primary_key=True, index=True)
-    hse_email = Column(String(100), unique=True, index=True, nullable=False)
-    telegram_id = Column(String(50), unique=True, nullable=True)
     role = Column(String(20), default="student")
     hashed_password = Column(String(255), nullable=False)
-    full_name = Column(String(200))
-    student_id = Column(String(50), nullable=True)
-    is_active = Column(Boolean, default=True)
-    profile_photo = Column(String(500), nullable=True)
-    created_at = Column(DateTime(timezone=True), server_default=func.now())
-    created_by = Column(Integer, nullable=True)
